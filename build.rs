66a24b4d3c8aeea3d75fd1d8ec2eaf77cfd80807
Alexander Chaplin Braz <contact@alexchaplinbraz.com>
2021-11-03 17:26:12 +0100

    Update dependencies

--- build.rs
+++ build.rs
@@ -4,9 +4,10 @@ fn main() {
 
     let ablavema32_file = File::open("extra/logo/ablavema32.png").unwrap();
     let decoder = Decoder::new(ablavema32_file);
-    let (info, mut reader) = decoder.read_info().unwrap();
-    let mut buf = vec![0; info.buffer_size()];
-    reader.next_frame(&mut buf).unwrap();
+    let mut reader = decoder.read_info().unwrap();
+    let buffer_size = reader.output_buffer_size();
+    let mut buf = vec![0; buffer_size];
+    let info = reader.next_frame(&mut buf).unwrap();
     let mut iced_icon_data_file = File::create("extra/temp/iced_icon_data").unwrap();
 
     iced_icon_data_file.write_all(&buf).unwrap();
